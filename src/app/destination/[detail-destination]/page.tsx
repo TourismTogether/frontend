a5ed62521a7a0b3bd235bec69e32d4eecbb94ce2
duckilot <prto2802@gmail.com>
2025-12-13 23:03:42 +0700

    xóa file thừa

--- src/app/destination/[detail-destination]/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>trang detail</div>
-  )
-}
-
-export default page
--- src/app/destination/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>trang chứa tất cả các destination</div>
-  )
-}
-
-export default page
